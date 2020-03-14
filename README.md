f39a19dd4c3214fc7c3862cc80972e3cfba7e153
sykuang <sykuang.tw@gmail.com>
2020-03-14 14:04:16 +0800

    Init

---
+++ README.md
@@ -0,0 +1,10 @@
+`alias ga="git add"`
+`alias gau="git add -u"`
+`alias gl="git log --graph --pretty=oneline --abbrev-commit"`
+`alias gst="git status"`
+`alias glp="git pull --rebase"`
+`alias gc="git commit"`
+`alias gca="git commit --amend"`
+`alias gcm="git commit -m"`
+`alias gp="git push"`
+`alias gpg="git push HEAD:refs/for/master"`
---
+++ git-cmd.zsh
@@ -0,0 +1,11 @@
+# Git related alias
+alias ga="git add"
+alias gau="git add -u"
+alias gl="git log --graph --pretty=oneline --abbrev-commit"
+alias gst="git status"
+alias glp="git pull --rebase"
+alias gc="git commit"
+alias gca="git commit --amend"
+alias gcm="git commit -m"
+alias gp="git push"
+alias gpg="git push HEAD:refs/for/master"
