11409b5a703ed4beed4a588807a9650388e46175
sykuang <sykuang.tw@gmail.com>
2020-03-17 23:28:17 +0800

    Refine the command

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
-`alias ga="git add"`
-`alias gau="git add -u"`
-`alias gl="git log --graph --pretty=oneline --abbrev-commit"`
-`alias gst="git status"`
-`alias glp="git pull --rebase"`
-`alias gc="git commit"`
-`alias gca="git commit --amend"`
-`alias gcm="git commit -m"`
-`alias gp="git push"`
-`alias gpg="git push HEAD:refs/for/master"`
+```
+alias ga="git add"
+alias gau="git add -u"
+alias gc="git commit"
+alias gch="git checkout"
+alias gca="git commit --amend"
+alias gcm="git commit -m"
+alias gd="git diff"
+alias gl="git log --graph --pretty=oneline --abbrev-commit"
+alias glp="git pull --rebase"
+alias gp="git push"
+alias gpg="git push origin HEAD:refs/for/master"
+alias grh="git reset --hard"
+alias gst="git status"
+
+alias r='repo'
+alias rra='repo rebase --auto-stash'
+alias rs='repo sync'
+alias rsrra='repo sync ; repo rebase --auto-stash'
+alias ru='repo upload'
+alias rst='repo status'
+alias rsto='repo status -o'
+alias rfa='repo forall -c'
+alias rfap='repo forall -p -c'
+alias rinf='repo info'
+```
--- git-cmd.zsh
+++ git-cmd.zsh
@@ -1,11 +1,30 @@
 # Git related alias
 alias ga="git add"
+alias gap="git add -p"
 alias gau="git add -u"
-alias gl="git log --graph --pretty=oneline --abbrev-commit"
-alias gst="git status"
-alias glp="git pull --rebase"
 alias gc="git commit"
+alias gch="git checkout"
 alias gca="git commit --amend"
 alias gcm="git commit -m"
+alias gd="git diff"
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias gls="git log --stat"
+alias glp="git log -p"
 alias gp="git push"
-alias gpg="git push HEAD:refs/for/master"
+alias grv="git review"
+alias grb="git rebase -i"
+alias gre="git reset --hard"
+alias gst="git status"
+
+# Repo related alias
+alias r='repo'
+alias rra='repo rebase --auto-stash'
+alias rd='repo download'
+alias rs='repo sync'
+alias rsrra='repo sync ; repo rebase --auto-stash'
+alias ru='repo upload'
+alias rst='repo status'
+alias rsto='repo status -o'
+alias rfa='repo forall -c'
+alias rfap='repo forall -p -c'
+alias rinf='repo info'
